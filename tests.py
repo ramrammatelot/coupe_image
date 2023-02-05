c5a6d03bb60bc499b6077ca97d813773ee7fe80f
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2023-02-05 18:35:43 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import skimage 
 ################################################
 # Ce programme découpe des images !?
-# pif pouf paf
+# pif pouf paf pif
 #####
 
 class pixel:
